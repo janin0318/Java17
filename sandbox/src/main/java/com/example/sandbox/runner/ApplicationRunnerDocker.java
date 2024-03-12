2e2b9fb9f1c9a2c830a12291a7b224d6f641308d
janin0318 <janin-ou@outlook.jp>
2024-03-12 23:29:01 +0900

    docker-compose時にテーブルとデータを作成するように修正

--- sandbox/src/main/java/com/example/sandbox/runner/ApplicationRunnerDocker.java
+++ sandbox/src/main/java/com/example/sandbox/runner/ApplicationRunnerDocker.java
@@ -1,5 +1,8 @@
 package com.example.sandbox.runner;
 
+import com.example.sandbox.dto.Shohin;
+import com.example.sandbox.service.ShohinService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,8 +14,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ApplicationRunnerDocker implements ApplicationRunner {
 
+  private final ShohinService shohinService;
+
+  ApplicationRunnerDocker(ShohinService shohinService) {
+    this.shohinService = shohinService;
+  }
+
   @Override
   public void run(ApplicationArguments args) {
     log.info("Profile : dockerの実施");
+    List<Shohin> shohinList = shohinService.getAllShohin();
+    log.info(shohinList.toString());
+    List<String> shohinMei = shohinService.getAllShohinMei();
+    log.info(shohinMei.toString());
+    Shohin shohin = shohinService.getShohinDetail("0002");
+    log.info(shohin.toString());
   }
 }
