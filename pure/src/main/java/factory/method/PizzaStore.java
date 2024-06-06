884faa9a3f4a099d5786073b251cab046c0c4bb9
janin0318 <janin-ou@outlook.jp>
2024-06-07 00:01:20 +0900

    Factoryパターン

---
+++ pure/src/main/java/factory/method/PizzaStore.java
@@ -0,0 +1,24 @@
+package factory.method;
+
+import factory.pizza.Pizza;
+
+public abstract class PizzaStore {
+
+  public Pizza orderPizza(String type) {
+
+    Pizza pizza;
+
+    pizza = createPizza(type);
+
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    pizza.box();
+
+    System.out.println("-----------------------");
+
+    return pizza;
+  }
+
+  protected abstract Pizza createPizza(String type);
+}
