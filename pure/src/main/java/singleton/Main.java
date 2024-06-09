5978907bcb76456e3f9af2c83ca965ffc96ba699
janin0318 <janin-ou@outlook.jp>
2024-06-09 20:15:52 +0900

    Singletonパターン

---
+++ pure/src/main/java/singleton/Main.java
@@ -0,0 +1,13 @@
+package singleton;
+
+import java.util.Objects;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ChocolateBoiler chocolateBoiler = ChocolateBoiler.getInstance();
+    System.out.println(Objects.isNull(chocolateBoiler));
+    chocolateBoiler.fill();
+  }
+
+}
